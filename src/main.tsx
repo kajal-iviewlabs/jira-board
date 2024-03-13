ec65288257547def9e53b80b0095b1e456f7e9dc
Kajal Kapadiya <kajalkapadiya@Kajals-MacBook-Air.local>
2024-03-13 15:40:28 +0530

    adding .env and .env.example file

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN || "default-domain";
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID || "default-client-id";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
-    clientId={process.env.QJkqyDj5PnDpB89efrDCgVhN4ryoZHUE || ""}
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
